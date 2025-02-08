80fa7d3c90bc640ceedfd06c9d515ec73f604107
Oak Platinum <oakplatinum@qq.com>
2025-02-08 18:10:30 +0800

    fix

--- misc/update.php
+++ misc/update.php
@@ -5,12 +5,10 @@ chdir($REPO_PATH);
 
 exec('git pull 2>&1', $output, $exitCode);
 
-$output = implode("\n", $output);
-
 if ($exitCode !== 0) {
     http_response_code(500);
-    echo "<pre>Failed to pull repository:\n$output</pre>";
 } else {
+    exec('nb stop', $output, $exitCode);
+    exec('nb run', $output, $exitCode);
     http_response_code(200);
-    echo "<pre>Repository updated successfully:\n$output</pre>";
 }
